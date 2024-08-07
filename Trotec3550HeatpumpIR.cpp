ed2880809c2bc2945c0eed4ae3aa5fa3bc6b85e6
Stephen <36908162+MrSJDDevelopments@users.noreply.github.com>
2024-08-07 18:12:00 +0100

    Code Update

--- Trotec3550HeatpumpIR.cpp
+++ Trotec3550HeatpumpIR.cpp
@@ -12,7 +12,7 @@ Trotec3550HeatpumpIR::Trotec3550HeatpumpIR() : HeatpumpIR()
 
 void Trotec3550HeatpumpIR::send(IRSender& IR, uint8_t powerModeCmd, uint8_t operatingModeCmd, uint8_t fanSpeedCmd, uint8_t temperatureCmd, uint8_t swingVCmd)
 { 
-ESP_LOGE("inside","send method called");
+ESP_LOGE("inside","sendTROTEC3550 called");
 
 //############### DEFAULT SETTINGS #################
 //################################################## 
