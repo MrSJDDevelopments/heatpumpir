2f0f114eee6f542e8f4f44280a92273a13c85805
Stephen <36908162+MrSJDDevelopments@users.noreply.github.com>
2024-08-07 21:30:57 +0100

    removal of non functional log

--- Trotec3550HeatpumpIR.cpp
+++ Trotec3550HeatpumpIR.cpp
@@ -4,7 +4,7 @@ Trotec3550HeatpumpIR::Trotec3550HeatpumpIR() : HeatpumpIR()
 {
   static const char model[] PROGMEM = "TROTEC3550";
   static const char info[]  PROGMEM = "{\"mdl\":\"Trotec3550\",\"dn\":\"TROTEC3550\",\"mT\":16,\"xT\":30,\"fs\":3}";
-LOGLN("inside","send method called");
+
   _model = model;
   _info = info;
 }
@@ -12,7 +12,7 @@ LOGLN("inside","send method called");
 
 void Trotec3550HeatpumpIR::send(IRSender& IR, uint8_t powerModeCmd, uint8_t operatingModeCmd, uint8_t fanSpeedCmd, uint8_t temperatureCmd, uint8_t swingVCmd)
 { 
-LOGLN("inside","send method called");
+
 //############### DEFAULT SETTINGS #################
 //################################################## 
 
@@ -70,7 +70,7 @@ LOGLN("inside","send method called");
 
 void Trotec3550HeatpumpIR::sendTROTEC3550(IRSender& IR, uint8_t powerMode, uint8_t operatingMode, uint8_t fanSpeed, uint8_t temperature, uint8_t swingV)
 {
-LOGLN("inside","sendTROTEC3550 called");
+
 //########################### Code Info Sourced From ############################
 //###############################################################################
 // https://github.com/crankyoldgit/IRremoteESP8266
